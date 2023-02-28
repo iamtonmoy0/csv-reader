296f57eb392d901d0279b1ebb5783238393cf285
iamtonmoy0 <tonmoy2code@gmail.com>
2023-02-28 14:41:52 -0500

    record print func added

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,21 @@ use std::error::Error;
 
 // csv import
 use csv;
+// read from file
+fn read_from_file(path:&str)->Result<(),Box<dyn Error>>{
+    let mut reader = csv::Reader::from_path(path)?;
+   for result in reader.records(){
+    let record= result?;
+    println!("{:?} ",record);
+   } 
+   Ok(())
+
+}
 
 fn main(){
+    if let Err(e)=read_from_file("./file.csv"){
+        eprintln!("{}",e);
+    }
 
-    
+
 }
